eaea12b9baf3cab46e1637fb7704046d85ee122e
FransiscusMichael <michaelpatty42@gmail.com>
2023-05-08 18:12:08 +0700

    commit michael ref #19

---
+++ client/src/components/EditKelas.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { getCourse } from "../controller/getCourse"
+import FormKelas from "./FormKelas"
+
+const EditKelas = () => {
+    const {id_kelas} = useParams()
+    const [editValue, setEditValue] = useState({namaKelas: '', tentangKelas: '', introductionKelas: '', kategoriKelas: '', materiKelas: '', hargaCoretKelas: 0, hargaAsliKelas: 0})
+    // const {namaKelas, tentangKelas, kategoriKelas, materiKelas, hargaCoretKelas, hargaAsliKelas} = addValue
+
+    const handleChange = e => {
+        setEditValue(preVal => {
+            return {
+                ...preVal,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
+    const fetchDetailKelas = async () => {
+        const data = await getCourse()
+        const kelas = data.find(item => item._id === '6458d639edd79b62a22ae3e2')
+        setEditValue(preVal => {
+            return {
+                ...preVal,
+                ...kelas
+            }
+        })
+    }
+
+    useEffect(() => {
+        fetchDetailKelas()
+    }, [])
+
+    return (
+        <div className="edit-kelas">
+            <div className="form-header">
+                <h1 className="form-title">Edit Kelas {editValue.namaKelas}</h1>
+                <p className="form-redirect">Atur konten kelas dan update sesuai kebutuhan</p>
+            </div>
+            <FormKelas handleChange={handleChange} handleSubmit={handleSubmit} kelasValue={editValue} cta='Update' />
+        </div>
+    )
+}
+
+export default EditKelas
