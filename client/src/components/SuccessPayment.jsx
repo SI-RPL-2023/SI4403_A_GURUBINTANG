ab5819bf2cceff9af3c1beab8053c4fa946e6df6
Usein <useinakbar01@gmail.com>
2023-05-28 16:37:28 +0700

    commit usein resolve #29

--- client/src/components/SuccessPayment.jsx
+++ client/src/components/SuccessPayment.jsx
@@ -17,7 +17,7 @@ const SuccessPayment = ({cookies}) => {
                 <h2 className="success__subtitle">Kamu telah berhasil melakukan pembelian. Selamat belajar dan tetap semangat. Mulai belajar sekarang!</h2>
                 <div className="success__cta-box">
                     <a href={`/materi/${id_user}/${id_kelas}`} className="success__cta-primary">Mulai Belajar</a>
-                    <a href={`/user/transaksi/${cookies.id}`} className="success__cta-secondary">Lihat Detail Transaksi</a>
+                    <a href={`/user/transaksi/${id_user}`} className="success__cta-secondary">Lihat Detail Transaksi</a>
                 </div>
             </div>
         </div>
